77b8014749ebd85e3e024a5633bde3d69fe7bdc4
Jan Bobisud <me@bobisjan.com>
2015-01-11 09:23:11 +0100

    Allow to change locale from template

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  selectedLocale: null,
+
+  init: function() {
+    this._super();
+    var code = this.get('locale.code');
+
+    this.set('selectedLocale', code);
+  },
+
+  locales: function() {
+    var locales = this.get('locale.locales');
+
+    return locales.toArray().filterBy('enabled', true).mapBy('code');
+  }.property('locale.locales.@each.enabled'),
+
+  selectedLocaleDidChange: function() {
+    var code = this.get('selectedLocale');
+    var locale = this.get('locale');
+
+    locale.set('code', code);
+  }.observes('selectedLocale')
+
+});

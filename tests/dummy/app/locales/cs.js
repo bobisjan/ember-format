d6710e9aa456a536ec9ea394bc789c596a9abe79
Jan Bobisud <me@bobisjan.com>
2015-01-11 09:35:36 +0100

    Add `cs` locale

---
+++ tests/dummy/app/locales/cs.js
@@ -0,0 +1,11 @@
+export default {
+  locales: 'cs',
+  messages: {
+    car: 'auto',
+    hello: 'Ahoj, {name}!',
+    photos: 'Máš {numPhotos, plural, =0 {žádnou fotku.} =1 {jednu fotkou.} =2 {dvě fotky} =3 {tři fotky} =4 {čtyři fotky} other {# fotek.}}'
+  },
+  formats: {
+    // Here you can pass custom defined options for format styles...
+  }
+};

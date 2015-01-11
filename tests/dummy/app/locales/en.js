cd0bfbe582db94ba1ea1a592dc46d2dbdf56d966
Jan Bobisud <me@bobisjan.com>
2015-01-11 09:13:16 +0100

    Add default locale

---
+++ tests/dummy/app/locales/en.js
@@ -0,0 +1,9 @@
+export default {
+  locales: 'en',
+  messages: {
+    // Here you can pass localized strings...
+  },
+  formats: {
+    // Here you can pass custom defined options for format styles...
+  }
+};

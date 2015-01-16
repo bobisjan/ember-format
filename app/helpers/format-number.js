00e87f5c9c9aea05c1186a843853bf89411c9d24
Jan Bobisud <me@bobisjan.com>
2015-01-16 18:06:00 +0100

    Add `format-number` helper

---
+++ app/helpers/format-number.js
@@ -0,0 +1,7 @@
+import formatHelper from './format';
+
+export default function formatNumberHelper(params, hash, options, env) {
+  Ember.assert('You passed `' + params.length + '` parameters in your template, but just input is required.', params.length === 1);
+  params.unshift('number');
+  return formatHelper.call(this, params, hash, options, env);
+}
--- app/initializers/helper.js
+++ app/initializers/helper.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 import formatHelper from '../helpers/format';
 import formatDateHelper from '../helpers/format-date';
 import formatMessageHelper from '../helpers/format-message';
+import formatNumberHelper from '../helpers/format-number';
 
 export function initialize(/*container, application*/) {
   Ember.HTMLBars.registerHelper('format', formatHelper);
   Ember.HTMLBars.registerHelper('format-date', formatDateHelper);
   Ember.HTMLBars.registerHelper('format-message', formatMessageHelper);
+  Ember.HTMLBars.registerHelper('format-number', formatNumberHelper);
 }
 
 export default {

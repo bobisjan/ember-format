ac3bdb5a2620f1dc39edaecf5f9c88176a4cbe9a
Jan Bobisud <me@bobisjan.com>
2015-01-16 18:04:47 +0100

    Add `format-date` helper

---
+++ app/helpers/format-date.js
@@ -0,0 +1,7 @@
+import formatHelper from './format';
+
+export default function formatDateHelper(params, hash, options, env) {
+  Ember.assert('You passed `' + params.length + '` parameters in your template, but just input is required.', params.length === 1);
+  params.unshift('date');
+  return formatHelper.call(this, params, hash, options, env);
+}
--- app/initializers/helper.js
+++ app/initializers/helper.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 import formatHelper from '../helpers/format';
+import formatDateHelper from '../helpers/format-date';
 
 export function initialize(/*container, application*/) {
   Ember.HTMLBars.registerHelper('format', formatHelper);
+  Ember.HTMLBars.registerHelper('format-date', formatDateHelper);
 }
 
 export default {

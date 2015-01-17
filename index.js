3292797dde5e0cbfdea5c7842f6127311eb958b2
Jan Bobisud <me@bobisjan.com>
2015-01-17 11:18:22 +0100

    Add command for extracting locale's codes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var commands = require('./lib/commands');
+
 module.exports = {
-  name: 'ember-format'
+  name: 'ember-format',
+
+  includedCommands: function() {
+    return commands;
+  }
 };

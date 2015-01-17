691d7e9545e27822dfd9d811a7688566a3ab4099
Jan Bobisud <me@bobisjan.com>
2015-01-17 15:21:03 +0100

    Extract CLDR data before install
    Allows to reject installation when invalid locale is provided.

--- blueprints/locale/index.js
+++ blueprints/locale/index.js
@@ -3,41 +3,76 @@ var fs = require('fs-extra');
 var path = require('path');
 var Promise = require('ember-cli/lib/ext/promise');
 var serialize = require('serialize-javascript');
+var SilentError = require('ember-cli/lib/errors/silent');
 var writeFile = Promise.denodeify(fs.outputFile);
 
 module.exports = {
-  description: 'Generate a locale',
+  description: 'Generate a locale with CLDR data',
 
-  localeFor: function(options) {
-    return options.entity.name;
+  normalizeEntityName: function(entityName) {
+    if (!this.isKnownLocaleCode(entityName)) {
+      throw new SilentError('The `ember generate locale` command does not ' +
+      'know locale `' + entityName + '`.');
+    }
+
+    return this.normalizeLocaleCode(entityName);
   },
 
   locals: function(options) {
-    var locale = this.localeFor(options);
+    var locale = this.localeCodeFor(options);
 
     return {
       locale: locale
     };
   },
 
-  afterInstall: function(options) {
-    var locale = this.localeFor(options);
-    var relativePath = path.join('app', 'locales', locale + '-cldr.js');
+  beforeInstall: function(options) {
+    var code = this.localeCodeFor(options);
+    var relativePath = path.join('app', 'locales', code + '-cldr.js');
     var fullPath = path.join(this.project.root, relativePath);
-    var data = {
-      locale: locale,
-      pluralRuleFunction: this.extractPluralRuleFunction(locale),
-      fields: this.extractFields(locale)
+
+    var locale = {
+      locale: code,
+      pluralRuleFunction: this.extractPluralRuleFunction(code),
+      fields: this.extractFields(code)
     }
-    var contents = 'export default ' + serialize(data) + ';';
+
+    if (!this.isSupportedLocale(locale)) {
+      return Promise.reject(new SilentError("The locale `" + code + "` is not supported."));
+    }
+
+    var contents = 'export default ' + serialize(locale) + ';';
 
     return writeFile(fullPath, contents);
   },
 
+  localeCodeFor: function(options) {
+    return this.normalizeLocaleCode(options.entity.name);
+  },
+
+  isKnownLocaleCode: function(code) {
+    if (!code || code === 'root') {
+      return false;
+    }
+
+    code = code.replace(/-/g, '_');
+    code = code.toLowerCase();
+
+    return cldr.localeIds.indexOf(code) >= 0;
+  },
+
+  isSupportedLocale: function(locale) {
+    return !!locale.pluralRuleFunction && !!locale.fields
+  },
+
+  normalizeLocaleCode: function(code) {
+    return code.replace(/_/g, '-');
+  },
+
   /**
-  https://github.com/yahoo/grunt-extract-cldr-data/blob/master/lib/extract.js
-  Copyright (c) 2014 Yahoo Inc.
-  Licensed under the Yahoo BSD license.
+    https://github.com/yahoo/grunt-extract-cldr-data/blob/master/lib/extract.js
+    Copyright (c) 2014 Yahoo Inc.
+    Licensed under the Yahoo BSD license.
   */
   extractPluralRuleFunction: function(locale) {
     var fn = cldr.extractPluralRuleFunction(locale).toString();
